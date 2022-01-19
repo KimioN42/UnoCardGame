382d00927ab52a18de784a1ab7a9249501c4ae3c
Kimio Nishino <kimionishino1@gmail.com>
2022-01-18 21:46:52 -0500

    Some refactoring on the Deck package to make it easier to read

--- src/deck/Deck.java
+++ src/deck/Deck.java
@@ -17,54 +17,34 @@ import deck.DeckDivision;
  */
 public class Deck {
     
-    private static final int red = 0;
-    private static final int green = 1;
-    private static final int blue = 2;
-    private static final int yellow = 3;
-    private static final int actionRed2 = 4;
-    private static final int actionGreen2 = 5;
-    private static final int actionBlue2 = 6;
-    private static final int actionYellow2 = 7;
-    private static final int actionRedSkip = 8;
-    private static final int actionGreenSkip = 9;
-    private static final int actionBlueSkip = 10;
-    private static final int actionYellowSkip = 11;
-    private static final int actionRedReverse = 12;
-    private static final int actionGreenReverse = 13;
-    private static final int actionBlueReverse = 14;
-    private static final int actionYellowReverse = 15;
-    private static final int actionWild = 16;
-    private static final int actionWildDraw4 = 17;
-    
-   
     private boolean fullDeck[][] = new boolean[18][];
 
     
     //constructor for the deck, should set every card in the deck to true or false
     //depending on if it is initialized on the table or on the player
     public Deck(boolean init) {
-        fullDeck[red] = new boolean[19];
-        fullDeck[green] = new boolean[19];
-        fullDeck[blue] = new boolean[19];
-        fullDeck[yellow] = new boolean[19];
+        fullDeck[DeckDivision.red] = new boolean[19];
+        fullDeck[DeckDivision.green] = new boolean[19];
+        fullDeck[DeckDivision.blue] = new boolean[19];
+        fullDeck[DeckDivision.yellow] = new boolean[19];
         
-        fullDeck[actionRed2] = new boolean[2];
-        fullDeck[actionGreen2] = new boolean[2];
-        fullDeck[actionBlue2] = new boolean[2];
-        fullDeck[actionYellow2] = new boolean[2];
+        fullDeck[DeckDivision.actionRed2] = new boolean[2];
+        fullDeck[DeckDivision.actionGreen2] = new boolean[2];
+        fullDeck[DeckDivision.actionBlue2] = new boolean[2];
+        fullDeck[DeckDivision.actionYellow2] = new boolean[2];
         
-        fullDeck[actionRedSkip] = new boolean[2];
-        fullDeck[actionGreenSkip] = new boolean[2];
-        fullDeck[actionBlueSkip] = new boolean[2];
-        fullDeck[actionYellowSkip] = new boolean[2];
+        fullDeck[DeckDivision.actionRedSkip] = new boolean[2];
+        fullDeck[DeckDivision.actionGreenSkip] = new boolean[2];
+        fullDeck[DeckDivision.actionBlueSkip] = new boolean[2];
+        fullDeck[DeckDivision.actionYellowSkip] = new boolean[2];
         
-        fullDeck[actionRedReverse] = new boolean[2];
-        fullDeck[actionGreenReverse] = new boolean[2];
-        fullDeck[actionBlueReverse] = new boolean[2];
-        fullDeck[actionYellowReverse] = new boolean[2];
+        fullDeck[DeckDivision.actionRedReverse] = new boolean[2];
+        fullDeck[DeckDivision.actionGreenReverse] = new boolean[2];
+        fullDeck[DeckDivision.actionBlueReverse] = new boolean[2];
+        fullDeck[DeckDivision.actionYellowReverse] = new boolean[2];
         
-        fullDeck[actionWild] = new boolean[4];
-        fullDeck[actionWildDraw4] = new boolean[4];
+        fullDeck[DeckDivision.actionWild] = new boolean[4];
+        fullDeck[DeckDivision.actionWildDraw4] = new boolean[4];
         
         for(int i = 0; i < 18; i++) {
             Arrays.fill(fullDeck[i], init);
